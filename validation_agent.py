79a0a52f59a9955587a7033a4b8eaa3e4921f6f0
Deep <deepd1534@gmail.com>
2025-05-09 23:16:16 +0530

    Raw Validation Agent added

---
+++ validation_agent.py
@@ -0,0 +1,15 @@
+from agno.agent import Agent
+from agno.models.google import Gemini
+from google import genai
+
+validation_agent = Agent(
+    model=Gemini(id="gemini-2.0-flash"),
+    instructions=[
+        "You are a validation agent. Your job is to validate the contents of the attached file.",
+        "You will be provided with a file and you need to validate its contents.",
+        "If the file is not valid, please provide a detailed explanation of why it is not valid.",
+        "If the file is valid, please confirm that it is valid.",
+    ]
+    markdown=True,
+    add_history_to_messages=True,
+)
